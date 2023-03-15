c0d928e490ff6c13a4f028e7f21b1fb4ee25bd7f
hgy_rick <gy_rick@foxmail.com>
2023-03-15 16:56:08 +0800

    🐳 chore: add vis pc

--- test/align.cpp
+++ test/align.cpp
@@ -3,6 +3,7 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
 #include <spdlog/spdlog.h>
 #include <spdlog/version.h>
@@ -53,16 +54,39 @@ int main(int argc, char const *argv[])
     
     Pose6d init_pose = Eigen::Isometry3d::Identity();
     
+    SPDLOG_INFO("target cloud size: {}", target_cloud->points.size());
+    SPDLOG_INFO("source cloud size: {}", source_cloud->points.size());
+
     time::tictoc tt;
     SPDLOG_INFO("start to downsample pc!");
     voxelDownSample(target_cloud, 0.1f);
     voxelDownSample(source_cloud, 0.1f);
     SPDLOG_INFO("downsample pc elapsed {:.6f}s", tt);
 
+    SPDLOG_INFO("--------- after downsample ---------");
+    SPDLOG_INFO("target cloud size: {}", target_cloud->points.size());
+    SPDLOG_INFO("source cloud size: {}", source_cloud->points.size());
+
     SPDLOG_INFO("start to scan2map!");
     tt.tic();
     pcr->scan2Map(source_cloud, target_cloud, init_pose);
     SPDLOG_INFO("scan to map elapsed {:.3f}s", tt);
+
+    std::stringstream ss;
+    ss << init_pose.matrix();
+    SPDLOG_INFO("trans: \n{}", ss.str());
+
+    // visualize
+    pcl::PointCloud<PointType>::Ptr aligned(pcl::make_shared<pcl::PointCloud<PointType>>());
+    pcl::transformPointCloud(*source_cloud, *aligned, init_pose.matrix().cast<float>());
+    pcl::visualization::PCLVisualizer vis("vis");
+    pcl::visualization::PointCloudColorHandlerCustom<PointType> target_handler(target_cloud, 255.0, 0.0, 0.0);  // r
+    pcl::visualization::PointCloudColorHandlerCustom<PointType> source_handler(source_cloud, 0.0, 255.0, 0.0);  // g
+    pcl::visualization::PointCloudColorHandlerCustom<PointType> aligned_handler(aligned, 0.0, 0.0, 255.0);      // b
+    vis.addPointCloud(target_cloud, target_handler, "target");
+    vis.addPointCloud(source_cloud, source_handler, "source");
+    vis.addPointCloud(aligned, aligned_handler, "aligned");
+    vis.spin();
     
     return 0;
 }
