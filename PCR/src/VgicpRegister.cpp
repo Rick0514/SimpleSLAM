46fb5d6736102baa452e1b8ec8881217d90a7605
hgy_rick <gy_rick@foxmail.com>
2023-05-16 18:23:38 +0800

    🐳 chore

--- PCR/src/VgicpRegister.cpp
+++ PCR/src/VgicpRegister.cpp
@@ -10,7 +10,7 @@ struct VgicpRegister::Vgicp
 
     Vgicp()
     {
-        vgicp_.setResolution(1.0);
+        vgicp_.setResolution(constant::numCores);
         vgicp_.setNumThreads(1);
     }
 };
